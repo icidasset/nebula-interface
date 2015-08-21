234f0a9c7ccbe6bb5d28d002343d3e8a11fd44db
Steven Vandevelde <icid.asset@gmail.com>
2015-08-21 15:47:02 +0200

    Add a NotFoundRoute

--- src/javascripts/lib/Routes.jsx
+++ src/javascripts/lib/Routes.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
-import { Route, DefaultRoute } from 'react-router';
+import { Route, DefaultRoute, NotFoundRoute } from 'react-router';
 
 import App from './App.jsx';
 
 import AboutPage from '../pages/AboutPage.jsx';
 import IndexPage from '../pages/IndexPage.jsx';
+import NotFoundPage from '../pages/NotFoundPage.jsx';
 
 
 const Routes = (
   <Route handler={App} path="/">
     <DefaultRoute handler={IndexPage} />
     <Route handler={AboutPage} path="/about" />
+
+    <NotFoundRoute handler={NotFoundPage}/>
   </Route>
 );
 
---
+++ src/javascripts/pages/NotFoundPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+export default class NotFoundPage {
+  render() {
+    return (
+      <main>
+        Uh oh, 404 *sad face*
+      </main>
+    );
+  }
+}
