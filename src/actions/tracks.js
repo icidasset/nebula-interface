f3f108cadb2e7fb6149b65e5ed266f315abb305c
Steven Vandevelde <icid.asset@gmail.com>
2015-10-15 19:39:00 +0200

    Update dependencies

--- src/actions/tracks.js
+++ src/actions/tracks.js
@@ -6,11 +6,25 @@ export function fetchTracks() {
   return (dispatch, getState) => {
     dispatch({ type: types.FETCH_TRACKS });
 
+    // retrieve tracks from Firebase
     base.fetch('tracks', {
       context: this,
       then: function(data) {
-        setTimeout(() => dispatch({ type: types.FETCH_TRACKS_DONE }), 5000);
+        let items = data;
+        dispatch(fetchTracksDone(items));
       }
+    });
+  };
+}
+
+
+export function saveTracks(tracks) {
+  return (dispatch, getState) => {
+    dispatch({ type: types.SAVE_TRACKS });
+
+    // save tracks on Firebase
+    base.post('tracks', {
+      data: tracks
     });
   };
 }
@@ -18,4 +32,11 @@ export function fetchTracks() {
 
 export function addTrack(attributes) {
   return { type: types.ADD_TRACK, attributes };
+}
+
+
+/// Private
+///
+function fetchTracksDone(items=[]) {
+  return { type: types.FETCH_TRACKS_DONE, items };
 }
