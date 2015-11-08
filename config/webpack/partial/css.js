0d186131c077e3f35c05c747d9fee6922c55a89a
Steven Vandevelde <icid.asset@gmail.com>
2015-11-09 00:24:46 +0100

    Fix normalize css include

--- config/webpack/partial/css.js
+++ config/webpack/partial/css.js
@@ -2,8 +2,15 @@ import ExtractTextPlugin from 'extract-text-webpack-plugin';
 
 import autoprefixer from 'autoprefixer';
 import csspartialimport from 'postcss-partial-import';
-import postcss_normalize from 'postcss-normalize';
+import nearest from 'find-nearest-file';
+import path from 'path';
+import postcssNormalize from 'postcss-normalize';
 import precss from 'precss';
+
+
+const root = path.dirname(
+  nearest('package.json')
+);
 
 
 export default function css() {
@@ -13,18 +20,43 @@ export default function css() {
     localIdentName: '[name]-[local]-[hash:base64:5]',
   };
 
-  const postcssLoaderConfig = {
-    parser: 'postcss-scss',
-  };
+  const loaderOptions = [
+    'style-loader',
+    `css-loader?${JSON.stringify(cssLoaderConfig)}` +
+      `!postcss-loader?`,
+  ];
+
+  const primaryFiles = [
+    path.join(root, 'src', 'main'),
+    path.join(root, 'src', 'static'),
+  ];
 
   return {
     module: {
       loaders: [{
         test: /\.(scss|sass)$/,
-        loader: ExtractTextPlugin.extract(
-          'style-loader',
-          `css-loader?${JSON.stringify(cssLoaderConfig)}` +
-            `!postcss-loader?${JSON.stringify(postcssLoaderConfig)}`
+        include: primaryFiles,
+        loader: ExtractTextPlugin.extract.apply(
+          this,
+          [
+            loaderOptions[0],
+            loaderOptions[1] + JSON.stringify({
+              pack: 'withNormalize',
+              parser: 'postcss-scss',
+            }),
+          ]
+        ),
+      }, {
+        test: /\.(scss|sass)$/,
+        exclude: primaryFiles,
+        loader: ExtractTextPlugin.extract.apply(
+          this,
+          [
+            loaderOptions[0],
+            loaderOptions[1] + JSON.stringify({
+              parser: 'postcss-scss',
+            }),
+          ]
         ),
       }, {
         test: /\.(css)$/,
@@ -36,12 +68,16 @@ export default function css() {
     },
 
     postcss() {
-      return [
-        postcss_normalize(),
+      const defaults = [
         csspartialimport({ extension: 'scss' }),
         precss,
         autoprefixer({ browsers: ['last 2 versions'] }),
       ];
+
+      return {
+        defaults: defaults,
+        withNormalize: [postcssNormalize()].concat(defaults),
+      };
     },
 
     plugins: [
