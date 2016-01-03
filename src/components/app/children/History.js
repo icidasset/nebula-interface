15f88a22a0c0c9d6508030684ab8d7d6579a99c3
Steven Vandevelde <icid.asset@gmail.com>
2016-01-03 13:36:47 +0100

    Queue and History components

--- src/components/app/children/History.js
+++ src/components/app/children/History.js
@@ -1,7 +1,45 @@
-import { createElement } from 'react';
+import { createElement, Component, PropTypes } from 'react';
 
-import Message from '../../Message';
-import Middle from '../../Middle';
+import ContentWithMenu from '../../ContentWithMenu';
+import List from '../../List';
 
 
-export default () => (<Middle><Message>TODO: History</Message></Middle>);
+class Queue extends Component {
+
+  render() {
+    const items = this.props.queue.history.slice(0).reverse().slice(0, 250).map((track, idx) => {
+      return {
+        key: idx,
+        title: (<span>
+          <strong style={{ fontWeight: '500' }}>{track.properties.title}</strong>
+          <span> &nbsp;&mdash;&nbsp; {track.properties.artist}</span>
+        </span>),
+      };
+    });
+
+    const actions = [];
+
+    return (
+      <ContentWithMenu
+        title="History"
+        menuItems={[]}
+      >
+        <List
+          items={items}
+          emptyIcon="list"
+          emptyMessage="Nothing has been played yet."
+          actions={actions}
+        />
+      </ContentWithMenu>
+    );
+  }
+
+}
+
+
+Queue.propTypes = {
+  queue: PropTypes.object.isRequired,
+};
+
+
+export default Queue;
--- src/components/app/children/Queue.js
+++ src/components/app/children/Queue.js
@@ -1,7 +1,54 @@
-import { createElement } from 'react';
+import { createElement, Component, PropTypes } from 'react';
 
-import Message from '../../Message';
-import Middle from '../../Middle';
+import ContentWithMenu from '../../ContentWithMenu';
+import List from '../../List';
 
 
-export default () => (<Middle><Message>TODO: Queue</Message></Middle>);
+class Queue extends Component {
+
+  render() {
+    const items = this.props.queue.items.map((track, idx) => {
+      return {
+        key: idx,
+        title: (<span>
+          <strong style={{ fontWeight: '500' }}>{track.properties.title}</strong>
+          <span> &nbsp;&mdash;&nbsp; {track.properties.artist}</span>
+        </span>),
+      };
+    });
+
+    const actions = [
+      {
+        key: 'remove',
+        label: 'Remove',
+        icon: 'trash',
+        clickHandler: () => {
+          alert('TODO - Implement delete-queue-item');
+        },
+      },
+    ];
+
+    return (
+      <ContentWithMenu
+        title="Queue"
+        menuItems={[]}
+      >
+        <List
+          items={items}
+          emptyIcon="list"
+          emptyMessage="Nothing in the queue yet."
+          actions={actions}
+        />
+      </ContentWithMenu>
+    );
+  }
+
+}
+
+
+Queue.propTypes = {
+  queue: PropTypes.object.isRequired,
+};
+
+
+export default Queue;
